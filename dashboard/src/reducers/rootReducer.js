2797cb1dda2685784a27e09621d485938a795c5e
MARCOS DE PAIVA <marcosdepaiva@macbook-pro.local>
2021-05-07 19:50:11 +0000

    devops commit

--- dashboard/src/reducers/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import { overReducer } from "./overReducer";
-
-const rootReducer = combineReducers({
-    overview: overReducer,
-});
-
-export default rootReducer;
